5036bc92bedc2dace92167c0cb371852389a0df3
mingli <mingli@192.168.31.212>
2017-12-13 18:25:48 +0100

    init employee

--- CoreJava1/src/Employee.java
+++ CoreJava1/src/Employee.java
@@ -1,5 +1,42 @@
+import java.time.LocalDate;
 
 public class Employee {
- 
+
+	/* Field */
 	private String name;
+	private double salary;
+	private LocalDate hireDay;
+
+	/* Constructor */
+	public Employee(String name, double salary, int year, int month, int day) {
+		this.name = name;
+		this.salary = salary;
+		this.hireDay = LocalDate.of(year, month, day);
+	}
+
+	/* Method */
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public LocalDate getHireDay() {
+		return hireDay;
+	}
+
+	public void setHireDay(LocalDate hireDay) {
+		this.hireDay = hireDay;
+	}
+
 }
