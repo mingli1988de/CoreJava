55e642f336aeb5407cf88e923dd82531e8218985
mingli <mingli@192.168.31.212>
2017-12-14 23:28:12 +0100

    14.12

--- CoreJava1/src/Employee.java
+++ CoreJava1/src/Employee.java
@@ -1,14 +1,20 @@
 import java.time.LocalDate;
+import java.util.Random;
 
 public class Employee {
 
 	/* Field */
-	private static int nextId = 1;
+	private static int nextId;
 	private int id;
 	private final String name;
 	private double salary;
 	private LocalDate hireDay;
 
+	static {
+		Random generator = new Random();
+		nextId = generator.nextInt(10000);
+	}
+	
 	/* Constructor */
 	public Employee(String name, double salary, int year, int month, int day) {
 		setId();
@@ -59,7 +65,7 @@ public class Employee {
 	/* Unit test */
 	public static void main(String[] args) {
 		Employee e = new Employee("Test One", 50000, 1990, 1, 1);
-		System.out.println(e.getId() + "" + e.getName());
+		System.out.println(e.getId() + " " + e.getName());
 	}
 
 }
